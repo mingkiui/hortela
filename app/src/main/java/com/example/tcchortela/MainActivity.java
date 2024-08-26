9a2eb281a8dacdfb9e0e17c330c43df48d99d2b7
venusisnow <vivioutofarea@gmail.com>
2024-08-26 08:28:43 -0300

    versão atualizada 26-08

--- app/src/main/java/com/example/tcchortela/MainActivity.java
+++ app/src/main/java/com/example/tcchortela/MainActivity.java
@@ -3,12 +3,18 @@ package com.example.tcchortela;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    private EditText etUsername, etEmail, etPassword;
+    private Button btnLogin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        
     }
 }
